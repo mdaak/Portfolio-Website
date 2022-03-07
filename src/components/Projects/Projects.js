a1882a649d6040918076dae46f8da02247f6be11
mdaak <md.aak3@gmail.com>
2022-03-07 14:55:29 +0600

    time line

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -34,7 +34,6 @@ const Projects = () => (
         </BlogCard>
       ))}
     </GridContainer>
-    Projects
   </Section>
 );
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,46 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have worked with a range technologies in the web developer world.
+      Form Back-end to Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+            <ListParagraph>
+              Explore with <br/>
+              React.js
+            </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+            <ListParagraph>
+              Explore with <br/>
+              Nede.js and Databases
+            </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        < DiZend size="3rem"/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+            <ListParagraph>
+              Explore with <br/>
+              React.js
+            </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
